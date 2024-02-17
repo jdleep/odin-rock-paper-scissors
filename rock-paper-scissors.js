c00ac868cd5587281d92d4d811adbcdc60c9499e
Jeff Leep <3838327+jdleep@users.noreply.github.com>
2024-02-17 14:42:23 -0500

    Create getComputerChoice() function

---
+++ rock-paper-scissors.js
@@ -0,0 +1,22 @@
+function getComputerChoice() {
+    //Get random number between 0 and 2
+    let random = floor(Math.random()*3);
+
+    let choice = '';
+
+    switch (random) {
+        case 0:
+            choice = 'ROCK';
+            break;
+        case 1:
+            choice = 'PAPER';
+            break;
+        case 2:
+            choice = 'SCISSORS';
+            break;
+        default:
+            console.error('Random number used for choice outside of 0-2');
+    }
+
+    return choice;
+}
