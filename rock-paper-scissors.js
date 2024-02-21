86998ca3d9be77024b78ffb75e729ee139800e75
Jeff Leep <3838327+jdleep@users.noreply.github.com>
2024-02-21 12:15:43 -0500

    Create playRound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -23,6 +23,7 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
 
+    // Default round result is "LOSE"
     let roundResult = 'LOSE';
 
     // Player tied with computer
@@ -41,7 +42,7 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection === 'ROCK') {
             roundResult = 'WIN';
         }
-    // player selection is: SCISSORS
+    // SCISSORS wins if computer chose PAPER
     } else {
         if (computerSelection === 'PAPER') {
             roundResult = 'WIN'
@@ -69,6 +70,7 @@ function createResultStatement(playerSelection, computerSelection, roundResult) 
             `You Lose! ${computerSelection} beats ${playerSelection}`;
             break;
         default:
+            console.error('roundResult outside of TIE, WIN, or LOSE given to function');
     }
 
     return resultStatement;
@@ -79,16 +81,57 @@ function capitalize(s)
     return s && s[0].toUpperCase() + s.slice(1).toLowerCase();
 }
 
-const gameSelections = ['ROCK', 'PAPER', 'SCISSORS']
+function validateSelection(playerSelection) {
+    const validSelections = new Set(['ROCK', 'PAPER', 'SCISSORS']);
+    let isValid = validSelections.has(playerSelection.toUpperCase());
+    return isValid;
+}
 
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let playerSelection = '';
+    let computerSelection = '';
+    let roundResult = '';
+    let roundResultStatement = '';
 
-let isWin = false;
+    for (let i = 1; i <= 5; i++) {
+        playerSelection = prompt(`Round ${i}: Please enter "ROCK", "PAPER", or "SCISSORS"`);
 
-for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-        console.log(`Player: ${gameSelections[i]},` +
-        ` Computer: ${gameSelections[j]}`);
-        console.log(playRound(gameSelections[i], gameSelections[j]));
-        console.log(createResultStatement(gameSelections[i], gameSelections[j], playRound(gameSelections[i], gameSelections[j])));
+        // Player canceled
+        if (playerSelection === null) {
+            console.log('Goodbye!');
+            return;
+        }
+
+        if (validateSelection(playerSelection)) {
+            computerSelection = getComputerChoice();
+            roundResult = playRound(playerSelection, computerSelection)
+            
+            if (roundResult === 'WIN') {
+                playerScore++;
+            } else if (roundResult == 'LOSE') {
+                computerScore++;
+            } else {
+                // If tie, nothing happens
+            }
+            let roundResultStatement = 
+                createResultStatement(playerSelection, computerSelection,
+                    roundResult);
+            console.log(`Round ${i}: ${roundResultStatement}`);
+        } else {
+            computerScore++;
+            console.log('Incorrect value! You lose!')
+        }
+    }
+
+    if (computerScore > playerScore) {
+        console.log(`You Lost the Game! Computer: ${computerScore}, Player: ${playerScore}`);
+    } else if (playerScore > computerScore) {
+        console.log(`You Won the Game! Computer: ${computerScore}, Player: ${playerScore}`);
+    } else {
+        console.log('You Tied with the Computer!');
     }
 }
+
+playGame();
